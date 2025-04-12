49a904289ef334231bba74ff2331e1aca21295f1
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-04-12 17:25:14 -0400

    Add planted texture to Crop and CropType
    Co-authored-by: Jadi Miller <jadilmiller@gmail.com>

--- project/crops/crop.gd
+++ project/crops/crop.gd
@@ -1,4 +1,4 @@
-extends AnimatedSprite3D
+extends Sprite3D
 
 var _crop_type: CropType
 
@@ -6,3 +6,5 @@ var _crop_type: CropType
 func set_crop_type(crop_type: CropType) -> void:
 	_crop_type = crop_type
 	%Label3D.text = _crop_type.name
+	texture = crop_type.planted_texture
+	offset.y = crop_type.planted_texture.get_height() / 2
--- project/crops/crop_type.gd
+++ project/crops/crop_type.gd
@@ -2,3 +2,4 @@ class_name CropType
 extends Resource
 
 @export var name: String
+@export var planted_texture: Texture2D
